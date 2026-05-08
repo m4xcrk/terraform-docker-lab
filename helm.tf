d213c4204b06af3a2891b83e118f8126ebbb301b
TEK <tb_mouni@outlook.com>
2026-05-08 14:23:27 +0545

    🏁 Graduation: Full-stack automated cluster with K8s, Helm, and Redis

--- helm.tf
+++ helm.tf
@@ -1,9 +1,8 @@
 resource "helm_release" "redis_db" {
   name       = "my-db"
-  repository = "https://charts.bitnami.com/bitnami"
+  repository = "https://bitnami.com"
   chart      = "redis"
 
-  # We use 'set' to make the app "Lite" for our 2GB RAM VMs
   set {
     name  = "master.persistence.enabled"
     value = "false"
